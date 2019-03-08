bbd3de0e47f68e3c525140df5a34ec0bc5f04c43
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-08 13:16:07 -0600

    Using two way data binding, property binding, and event binding ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-form/main-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-form',
+  templateUrl: './main-form.component.html',
+  styleUrls: ['./main-form.component.css']
+})
+export class MainFormComponent implements OnInit {
+  userName = "";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  resetUser(){
+    this.userName = "";
+  }
+}
